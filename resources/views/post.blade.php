22d8403a0d946d767d4ff6f5d0a8dfd5b89676b8
rianasridamanik <you@example.com>
2025-06-24 02:55:43 +0700

    Simulasi Blog Posts

---
+++ resources/views/post.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.main')
+
+@section('container')
+
+<article>
+<h2>Judul</h2>
+<h5>Author</h5>
+<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non at voluptatem consectetur fugiat, saepe similique est quos culpa, eligendi iste excepturi distinctio quasi reprehenderit nulla ducimus asperiores animi et dolor!</p>
+</article>
+
+<a href="/blog">Back to Posts</a>
+
+@endsection
