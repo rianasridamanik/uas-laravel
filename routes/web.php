f637c8167bb584ebd45bc3fe453b9bc3f0d59762
rianasridamanik <you@example.com>
2025-06-24 01:24:23 +0700

    Layouting System

--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,21 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        "title" => "Home"
+    ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
+        "title" => "About",
         "name" => "Rian Asri Damanik",
         "email" => "rianasridamanik0@gmail.com"
     ]);
 });
 
 Route::get('/blog', function () {
-    return view('posts');
+    return view('posts', [
+        "title" => "Posts"
+    ]);
 });
