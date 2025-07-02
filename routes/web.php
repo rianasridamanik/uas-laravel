8403649057c1648fb6d293f107ebbc02b2e08d57
rianasridamanik <you@example.com>
2025-07-03 01:13:50 +0700

    Membuat Controller

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 
 Route::get('/', function () {
@@ -20,21 +21,5 @@ Route::get('/about', function () {
 
 
 
-Route::get('/blog', function () {
-     return view('posts', [
-        "title" => "Posts",
-        "posts" => Post::all() 
-    ]);
-});
-
-
-// halaman single post
-Route::get('posts/{slug}', function($slug) {
-    
-
-
-        return view('post', [
-            "title" => "Single Post",
-            "post" => Post::find($slug)
-        ]);
-});
+Route::get('/blog', [PostController::class, 'index']); 
+Route::get('posts/{slug}', [PostController::class, 'show']);
